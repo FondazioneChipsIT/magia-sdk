403f9856e2872baf7f5cbc8a70258a28d7dbfae9
callme-sam <219649100+callme-sam@users.noreply.github.com>
2026-03-23 09:00:30 +0100

    [test/spatz_on_magia] onnx_softmax: add more debug prints

--- tests/spatz_on_magia/onnx_softmax/spatz_task/onnx_softmax_task.c
+++ tests/spatz_on_magia/onnx_softmax/spatz_task/onnx_softmax_task.c
@@ -1,7 +1,21 @@
 #include "tile.h"
 #include "onnx_softmax_params.h"
 
-static inline _Float16 find_max(const _Float16 *vec, size_t len)
+static inline uint16_t get_raw(const _Float16 val)
+{
+    uint16_t raw;
+    memcpy(&raw, &val, sizeof(raw));
+    return raw;
+}
+
+static inline void print_vector_raw(const _Float16 *vec, size_t len)
+{
+    for (size_t i = 0; i < len; i++) {
+        printf("%d) %x\n", i, get_raw(vec[i]));
+    }
+}
+
+static __attribute__((noinline)) _Float16 find_max(const _Float16 *vec, size_t len)
 {
     const _Float16 *p_vec;
     _Float16 max;
@@ -17,7 +31,7 @@ static inline _Float16 find_max(const _Float16 *vec, size_t len)
     avl = len;
 
     asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(avl));
-    asm volatile ("vfmv.s.f v0, %0" :: "f"(max));
+    asm volatile ("vfmv.v.f v0, %0" :: "f"(max));
     asm volatile ("vfmv.v.f v8, %0" :: "f"(max));
 
     for(; avl > 0; avl -= vl) {
@@ -35,7 +49,7 @@ static inline _Float16 find_max(const _Float16 *vec, size_t len)
     return max;
 }
 
-static inline _Float16 compute_exponential_sum_fastexp_scalar(const _Float16 *src, _Float16 *dst, size_t len, _Float16 max)
+static __attribute__((noinline)) _Float16 compute_exponential_sum_fastexp_scalar(const _Float16 *src, _Float16 *dst, size_t len, _Float16 max)
 {
     _Float16 COEF = 1486.0f;
     _Float16 BIAS = 15360.0f;
@@ -66,10 +80,8 @@ static inline _Float16 compute_exponential_sum_fastexp_scalar(const _Float16 *sr
  * COEF = 2^mantissa / ln(2) (mantissa = 10 for FP16), scales x to the FP16 exponent range.
  * BIAS = exponent_bias * 2^mantissa (bias = 15 for FP16), shifts the bits to approximate exp.
  */
-static inline _Float16 compute_exponential_sum_fastexp(const _Float16 *src, _Float16 *dst, size_t len, _Float16 max)
+static __attribute__((noinline)) _Float16 compute_exponential_sum_fastexp(const _Float16 *src, _Float16 *dst, size_t len, _Float16 max)
 {
-    // printf("Vectorial\n");
-
     _Float16 COEF = 1486.0f;
     _Float16 BIAS = 15360.0f;
     _Float16 ZERO_f = 0.0f;
@@ -113,13 +125,13 @@ static inline _Float16 compute_exponential_sum_fastexp(const _Float16 *src, _Flo
     }
 
     asm volatile ("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(vl) : "r"(original_avl));
-    asm volatile ("vfredusum.vs v8, v0, v8");
+    asm volatile ("vfredosum.vs v8, v0, v8");
     asm volatile ("vfmv.f.s %0, v8" : "=f"(sum));
 
     return sum;
 }
 
-static inline void normalize(_Float16 *dst, size_t len, _Float16 sum)
+static __attribute__((noinline)) void normalize(_Float16 *dst, size_t len, _Float16 sum)
 {
     _Float16 *p_dst;
     size_t avl;
@@ -147,8 +159,8 @@ int onnx_softmax_task(void)
     uintptr_t params_addr;
     _Float16 *src;
     _Float16 *dst;
-    _Float16 max;
-    _Float16 sum;
+    volatile _Float16 max;
+    volatile _Float16 sum;
     size_t len;
 
     params_addr = mmio32(SPATZ_DATA);
@@ -158,9 +170,16 @@ int onnx_softmax_task(void)
     dst = (_Float16 *)params->addr_res;
     len = params->len;
 
+    printf("len=%u\n", len);
+    // print_vector_raw(src, len);
+
     max = find_max(src, len);
     sum = compute_exponential_sum_fastexp(src, dst, len, max);
     normalize(dst, len, sum);
+
+                                        /* -O0       -O1    -O2    -O3  */
+    // printf("max:%x\n", get_raw(max));   /* 42f1     42f1    42f1   42f1 */
+    // printf("sum:%x\n", get_raw(sum));   /* 566d     664a    664a   664a */
 
     return 0;
 }
